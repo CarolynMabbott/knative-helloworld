5737cdb24b243b7b8529a0281bcc9b09649af042
Adam Roberts <AROBERTS@uk.ibm.com>
2019-03-07 09:52:21 +0000

    Update helloworld.go

--- helloworld.go
+++ helloworld.go
@@ -8,7 +8,7 @@ import (
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	log.Print("Hello world received a request.......")
+	log.Print("Hello world received a request.........")
 	target := os.Getenv("TARGET")
 	if target == "" {
 		target = "World"
